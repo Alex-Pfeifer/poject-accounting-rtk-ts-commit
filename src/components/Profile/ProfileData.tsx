1e65166d15372ece4bbedc8e40f8bc92259af48b
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-04-13 15:00:14 +0200

    5 ProfileData

---
+++ src/components/Profile/ProfileData.tsx
@@ -0,0 +1,14 @@
+const ProfileData = () => {
+    return (
+        <>
+            <p>First name?</p>
+            <p>Last name</p>
+            <p>????</p>
+            <ul>
+                <li>?????????</li>
+            </ul>
+        </>
+    );
+};
+
+export default ProfileData;
