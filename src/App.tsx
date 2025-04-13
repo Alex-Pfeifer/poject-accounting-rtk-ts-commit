b27aa861ec2a870118bc3c2b4227f8d9c6071287
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-04-13 15:52:25 +0200

    10 main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
-import Guest from "./components/Guest";
+import Profile from "./components/Profile";
+import {Navigate, Route, Routes} from "react-router";
+import Guest from "../../poject-accounting-rtk-ts-commit/src/components/Guest";
 
 function App() {
+    const token = '';
+
 
     return (
-        <>
-            <Guest/>
-        </>
+        <Routes>
+            <Route path={"/"} element={token ? <Navigate to={'/profile'}/> : <Guest/>} />
+            <Route path={"/profile"} element={token ? <Profile/> : <Navigate to={'/'}/>} />
+        </Routes>
     )
 }
 
