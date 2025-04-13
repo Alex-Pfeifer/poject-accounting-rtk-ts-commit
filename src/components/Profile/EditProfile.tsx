4acb422393a4f49be8f7577b928f91f74102891a
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-04-13 15:37:22 +0200

    9 EditProfile

---
+++ src/components/Profile/EditProfile.tsx
@@ -0,0 +1,49 @@
+import {useState} from "react";
+
+interface Props {
+    close: () => void;
+}
+
+const EditProfile = ({close}: Props) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const handelClickSave = () => {
+        //TODO
+        alert('Save profile data')
+    }
+
+
+    const handelCkickClear = () => {
+        setFirstName('');
+        setLastName('');
+    }
+
+
+    return (
+        <>
+            <label>
+                First Name:
+                <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </label>
+            <label>
+                Last Name:
+                <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </label>
+            <button onClick={handelCkickClear}>Save and Close</button>
+            <button onClick={close}>Close without Save</button>
+            <button onClick={handelClickSave}>Clear</button>
+
+        </>
+    );
+};
+
+export default EditProfile;
